5c0b9bc62df61ba7cc9db518da2a5e168a415cd4
Philip Olsson <philip.olsson3@gmail.com>
2024-05-27 16:55:59 +0200

    Feature: Star rating almost working, creating restore point for debugging

--- src/index.ts
+++ src/index.ts
@@ -33,42 +33,85 @@ const updateVideo = async (id: number, changes: { title?: string; grade?: number
 
   return await response.json();
 };
+const createGradeStars = (
+  videoId: number,
+  currentGrade: number,
+  maxGrade: number,
+  onChange: (event: Event) => Promise<void>
+) => {
+  // inspired by https://codepen.io/jrsdiniz/pen/OJVdXjx
+  const stars = document.createElement("div");
+  stars.classList.add("stars");
+  stars.id = `stars_${videoId}`;
+  Array.apply(null, Array(maxGrade)).forEach((element, index) => {
+    const input = document.createElement("input");
+    input.type = "radio";
+    input.name = "grade";
+    input.value = (index + 1).toString();
+    input.checked = currentGrade === index + 1;
+    input.id = `star${index + 1}`;
+    stars.appendChild(input);
+    const label = document.createElement("label");
+    label.classList.add("star");
+    if (index + 1 === currentGrade) label.classList.add("active");
+    input.onchange = onChange;
+
+    label.htmlFor = `star${index + 1}`;
+    label.ariaHidden = "true";
+    label.title = `${index + 1} star`;
+    stars.appendChild(label);
+  });
+
+  return stars;
+};
 
 //Might constitute performance issues if the list is too long
 const createVideoList = (videos: Video[]) => {
   const videoList = document.getElementById("video_list");
   if (videoList) {
     videoList.innerHTML = "";
-    videos.forEach((video) => {
+    videos.forEach((_video, index) => {
+      const video = videos[index];
       const posterUrl = posterPlaceholder;
       //change posterUrl when the api is extended
 
       const videoElement = document.createElement("div");
+      videoElement.id = `video_${video.id}`;
       videoElement.classList.add("video");
       videoElement.innerHTML = `
-        <img src="${posterUrl}" />
+        <img src="${posterUrl}" alt="Poster for ${video.title}"/>
         <h2>${video.title}</h2>
       `;
-      const inputElement = document.createElement("input");
-      inputElement.type = "number";
-      inputElement.min = "1";
-      inputElement.max = "5";
-      inputElement.value = video.grade.toString();
-      inputElement.onchange = onGradeChange;
-      inputElement.dataset.id = video.id.toString();
 
-      videoElement.appendChild(inputElement);
+      const stars = createGradeStars(video.id, video.grade, 5, async (event) => {
+        const target = event.target as HTMLInputElement;
+        console.log(video);
+        if (!target || target.disabled) return;
+        console.log(target);
+        target.disabled = true;
+        target.parentElement?.classList.add("disabled");
+        await handleGradeChange(video.id, parseInt(target.value));
+        target.parentElement?.classList.remove("disabled");
+      });
+      videoElement.appendChild(stars);
       videoList.appendChild(videoElement);
     });
   }
 };
 
-const onGradeChange = async (event: Event) => {
-  const target = event.target as HTMLInputElement;
-
-  if (!target || typeof target.dataset.id === "undefined") return;
-  updateVideo(parseInt(target.dataset.id), { grade: parseInt(target.value) });
+const handleGradeChange = async (videoId: number, newGrade: number) => {
+  updateVideo(videoId, { grade: newGrade });
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   // Make sure FE data is reflecting BE data here
+};
+
+const handleSearch = async (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  if (target.disabled) return;
+  target.disabled = true;
+  const videos = await getVideos(target.value);
+  createVideoList(videos);
+  target.disabled = false;
 };
 
 const init = async () => {
@@ -77,18 +120,7 @@ const init = async () => {
   //Maybe do this as a form with a button and handle the submit event instead
   const searchInput = document.getElementById("search_input");
   if (searchInput) {
-    searchInput.onkeydown = async (event) => {
-      const target = event.target as HTMLInputElement;
-      if (event.key === "Enter") {
-        //Prevent further requests to avoid race conditions.
-        // Alternatives would be, cancel previous request, discard all but last request,
-        // queue the latest query, debounce the input, or a combination depending intended UX
-        target.disabled = true;
-        const videos = await getVideos(target.value);
-        createVideoList(videos);
-        target.disabled = false;
-      }
-    };
+    searchInput.onkeydown = handleSearch;
   }
 };
 
